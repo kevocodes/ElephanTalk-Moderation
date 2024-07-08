1171ebe6b196b3ab0b9fdd60b96ab67e3b3f87f1
kikicodes23 <00117420@uca.edu.sv>
2024-07-08 00:59:51 -0600

    fix: resolve bug on navbar file name import :bug:

--- elephantalk-moderation/src/app/page.js
+++ elephantalk-moderation/src/app/page.js
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import { Navbar } from '@/components/interface/navbar';
-import { MonitorModal } from '@/components/interface/MonitorModal';
-import { GetReports, GetReportsByType } from '@/services/api';
-import { formatDate } from '@/utils/formatDate';
-import { useSession } from 'next-auth/react';
-import React from 'react';
-import { LuEye } from 'react-icons/lu';
+import { Navbar } from "@/components/interface/Navbar";
+import { MonitorModal } from "@/components/interface/MonitorModal";
+import { GetReports, GetReportsByType } from "@/services/api";
+import { formatDate } from "@/utils/formatDate";
+import { useSession } from "next-auth/react";
+import React from "react";
+import { LuEye } from "react-icons/lu";
 import ClipLoader from "react-spinners/ClipLoader";
-import { DropdownMenu } from '@/components/interface/DropdownMenu';
-import { SortIcon } from '@/components/interface/SortIcon';
+import { DropdownMenu } from "@/components/interface/DropdownMenu";
+import { SortIcon } from "@/components/interface/SortIcon";
 
 export default function Home() {
-  const [activePage, setActivePage] = React.useState('monitor');
+  const [activePage, setActivePage] = React.useState("monitor");
   const [page, setPage] = React.useState(1);
   const [pages, setPages] = React.useState(1);
   const [reports, setReports] = React.useState([]);
@@ -22,12 +22,12 @@ export default function Home() {
   const [refresh, setRefresh] = React.useState(false);
   const [monitorOpen, setMonitorOpen] = React.useState(false);
   const [historyOpen, setHistoryOpen] = React.useState(false);
-  const [selectedOption, setSelectedOption] = React.useState('')
+  const [selectedOption, setSelectedOption] = React.useState("");
   const session = useSession();
   //estado para poder cambiar el filtrado de los reportes
-  const [optionToFilter, setOptionToFilter] = React.useState('')
+  const [optionToFilter, setOptionToFilter] = React.useState("");
   //estado para identificar cambio en ordenamiento por defecto estará ordenado en sentido ascendente
-  const [clickEvent, setClickEvent] = React.useState(false)
+  const [clickEvent, setClickEvent] = React.useState(false);
 
   React.useEffect(() => {
     if (!session?.data?.accessToken) {
@@ -37,26 +37,26 @@ export default function Home() {
     if (activePage === "monitor") {
       const fetchReports = async () => {
         try {
-          const query = new URLSearchParams()
-          query.append("limit", 10)
-          query.append("page", page)
+          const query = new URLSearchParams();
+          query.append("limit", 10);
+          query.append("page", page);
 
           if (optionToFilter) {
-            query.append("type", optionToFilter)
+            query.append("type", optionToFilter);
             if (clickEvent == true) {
-              query.append("order", "desc")
+              query.append("order", "desc");
             } else if (clickEvent == false) {
-              query.append("order", "asc")
+              query.append("order", "asc");
             }
           } else if (clickEvent == true) {
-            query.append("order", "desc")
+            query.append("order", "desc");
             if (optionToFilter) {
-              query.append("type", optionToFilter)
+              query.append("type", optionToFilter);
             }
           } else if (clickEvent == false) {
-            query.append("order", "asc")
+            query.append("order", "asc");
             if (optionToFilter) {
-              query.append("type", optionToFilter)
+              query.append("type", optionToFilter);
             }
           }
 
@@ -76,7 +76,6 @@ export default function Home() {
           if (refresh) {
             setRefresh(false);
           }
-
         } catch (error) {
           setLoading(false);
           //showAlert("Oops try again later...", "error");
@@ -84,30 +83,29 @@ export default function Home() {
       };
 
       fetchReports();
-    }
-    else if (activePage === "history") {
+    } else if (activePage === "history") {
       const fetchReports = async () => {
         try {
-          const query = new URLSearchParams()
-          query.append("limit", 10)
-          query.append("page", page)
+          const query = new URLSearchParams();
+          query.append("limit", 10);
+          query.append("page", page);
 
           if (optionToFilter) {
-            query.append("type", optionToFilter)
+            query.append("type", optionToFilter);
             if (clickEvent == true) {
-              query.append("order", "desc")
+              query.append("order", "desc");
             } else if (clickEvent == false) {
-              query.append("order", "asc")
+              query.append("order", "asc");
             }
           } else if (clickEvent == true) {
-            query.append("order", "desc")
+            query.append("order", "desc");
             if (optionToFilter) {
-              query.append("type", optionToFilter)
+              query.append("type", optionToFilter);
             }
           } else if (clickEvent == false) {
-            query.append("order", "asc")
+            query.append("order", "asc");
             if (optionToFilter) {
-              query.append("type", optionToFilter)
+              query.append("type", optionToFilter);
             }
           }
 
@@ -117,7 +115,6 @@ export default function Home() {
             endpoint: "history",
             query: query.toString(),
           });
-
 
           setReports(data?.reports);
           setPages(data?.pagination.pages);
@@ -136,7 +133,6 @@ export default function Home() {
     }
   }, [session, activePage, page, refresh, optionToFilter, clickEvent]);
 
-
   React.useEffect(() => {
     if (selectedRep === "") {
       return;
@@ -152,20 +148,15 @@ export default function Home() {
         if (activePage === "monitor") {
           setReport(report?.data);
           setMonitorOpen(true);
-        }
-        else if (activePage === "history") {
+        } else if (activePage === "history") {
           setReport(report?.data);
-          setHistoryOpen(true)
+          setHistoryOpen(true);
         }
-
-      } catch (error) {
-
-      }
+      } catch (error) {}
     };
 
     fetchReport();
-
-  }, [selectedRep])
+  }, [selectedRep]);
 
   const loadNext = () => {
     setPage((page) => page + 1);
@@ -173,10 +164,10 @@ export default function Home() {
 
   const loadLast = () => {
     setPage((page) => page - 1);
-  }
+  };
 
   //funciones para renderizar las opciones del filtro y además obtener el valor de estas opciones
-  const selectOptions = ['Seleccionar...', 'Todos', 'Tipo de reporte']
+  const selectOptions = ["Seleccionar...", "Todos", "Tipo de reporte"];
 
   function renderOptions() {
     return selectOptions.map((option, index) => {
@@ -184,62 +175,67 @@ export default function Home() {
         <option key={index} value={option}>
           {option}
         </option>
-      )
-    })
+      );
+    });
   }
 
-  //estado para poder abrir y cerrar el dropdownMenu 
-  const [visibility, setVisibility] = React.useState(false)
+  //estado para poder abrir y cerrar el dropdownMenu
+  const [visibility, setVisibility] = React.useState(false);
 
   const hideDropdown = () => {
-    setVisibility(false)
-    setSelectedOption('')
-  }
+    setVisibility(false);
+    setSelectedOption("");
+  };
 
   //función para obtener el valor del select que es el filtro
   function obtainSelectValue(e) {
     if (e.target.value === "Tipo de reporte") {
-      setSelectedOption(e.target.value)
-      setVisibility(true)
+      setSelectedOption(e.target.value);
+      setVisibility(true);
     } else if (e.target.value === "Todos") {
-      setSelectedOption('')
-      setOptionToFilter('')
+      setSelectedOption("");
+      setOptionToFilter("");
     }
   }
 
   //función para obtener la opción seleccionada del dropdownMenu
   function obtainOptionFilter(option) {
-    console.log(visibility)
-    console.log(selectedOption)
-    setSelectedOption('')
-    setOptionToFilter(option)
-    hideDropdown()
+    console.log(visibility);
+    console.log(selectedOption);
+    setSelectedOption("");
+    setOptionToFilter(option);
+    hideDropdown();
   }
 
   //función para determinar si se ha hecho click sobre el ícono de ordenamiento
   function handleIcon(click) {
-    setClickEvent(!click)
+    setClickEvent(!click);
   }
 
   return (
     <>
       <div className="min-h-screen bg-gray-100">
-        <Navbar
-          activePage={activePage}
-          setActivePage={setActivePage}
-        />
+        <Navbar activePage={activePage} setActivePage={setActivePage} />
         <main className="p-4">
           <h2 className="text-xl font-semibold mb-4 text-darkprim">
-            {activePage === "monitor" ? "Monitor de reportes" : "Historial de reportes"}
+            {activePage === "monitor"
+              ? "Monitor de reportes"
+              : "Historial de reportes"}
           </h2>
 
           <div className="flex justify-end mb-4">
-            <select className="p-2 bg-white border border-gray-300 rounded-md shadow-sm"
-              onClick={obtainSelectValue}>
+            <select
+              className="p-2 bg-white border border-gray-300 rounded-md shadow-sm"
+              onClick={obtainSelectValue}
+            >
               {renderOptions()}
             </select>
-            <DropdownMenu selectedOption={selectedOption} options={['post', 'comment']}
-              obtainOption={obtainOptionFilter} isVisible={visibility} invisible={hideDropdown}
+            <DropdownMenu
+              selectedOption={selectedOption}
+              options={["post", "comment"]}
+              obtainOption={obtainOptionFilter}
+              isVisible={visibility}
+              invisible={hideDropdown}
             />
           </div>
           <div className="bg-white shadow-md rounded-md overflow-hidden">
@@ -251,19 +247,21 @@ export default function Home() {
             />
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-primary">
-                <tr className='text-white'>
+                <tr className="text-white">
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">
                     Usuario
                   </th>
                   <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">
                     Tipo de reporte
                   </th>
-                  <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider flex
-                  justify-center items-center gap-3">
+                  <th
+                    className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider flex
+                  justify-center items-center gap-3"
+                  >
                     Fecha de creación
                     <SortIcon eventClick={handleIcon} />
                   </th>
-                  {(activePage === "history") && (
+                  {activePage === "history" && (
                     <>
                       <th className="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">
                         Decisión
@@ -276,14 +274,12 @@ export default function Home() {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200 text-gray-500">
-                {(!loading) && (<>
-                  {(activePage === "monitor") && (
-                    <>
-                      {
-                        reports?.map(({ _id, user, type, createdAt }, i) => (
-                          <tr
-                            key={`${i} - ${createdAt}`}
-                          >
+                {!loading && (
+                  <>
+                    {activePage === "monitor" && (
+                      <>
+                        {reports?.map(({ _id, user, type, createdAt }, i) => (
+                          <tr key={`${i} - ${createdAt}`}>
                             <td className="px-6 py-4 text-center whitespace-nowrap">
                               {user.username}
                             </td>
@@ -294,72 +290,73 @@ export default function Home() {
                               {formatDate(createdAt)}
                             </td>
                             <td className="px-6 py-4 flex justify-center items-center whitespace-nowrap">
-                              <LuEye className="text-gray-500 hover:text-primary"
+                              <LuEye
+                                className="text-gray-500 hover:text-primary"
                                 onClick={() => setSelectedRep(_id)}
                               />
                             </td>
                           </tr>
-                        ))
-                      }
-                    </>
-                  )}
-                  {(activePage === "history") && (
-                    <>
-                      {
-                        reports?.map(({ _id, user, type, createdAt, status }, i) => (
-                          <tr
-                            key={`${i} - ${createdAt}`}
-                          >
-                            <td className="px-6 py-4 text-center whitespace-nowrap">
-                              {user.username}
-                            </td>
-                            <td className="px-6 py-4 text-center whitespace-nowrap">
-                              {type}
-                            </td>
-                            <td className="px-6 py-4 text-center whitespace-nowrap">
-                              {formatDate(createdAt)}
-                            </td>
-                            <td className="px-6 py-4 text-center whitespace-nowrap">
-                              {status}
-                            </td>
-                            <td className="px-6 py-4 flex justify-center items-center whitespace-nowrap">
-                              <LuEye className="text-gray-500 hover:text-primary"
-                                onClick={() => setSelectedRep(_id)}
-                              />
-                            </td>
-                          </tr>
-                        ))
-                      }
-                    </>
-                  )}
-                </>)
-                }
+                        ))}
+                      </>
+                    )}
+                    {activePage === "history" && (
+                      <>
+                        {reports?.map(
+                          ({ _id, user, type, createdAt, status }, i) => (
+                            <tr key={`${i} - ${createdAt}`}>
+                              <td className="px-6 py-4 text-center whitespace-nowrap">
+                                {user.username}
+                              </td>
+                              <td className="px-6 py-4 text-center whitespace-nowrap">
+                                {type}
+                              </td>
+                              <td className="px-6 py-4 text-center whitespace-nowrap">
+                                {formatDate(createdAt)}
+                              </td>
+                              <td className="px-6 py-4 text-center whitespace-nowrap">
+                                {status}
+                              </td>
+                              <td className="px-6 py-4 flex justify-center items-center whitespace-nowrap">
+                                <LuEye
+                                  className="text-gray-500 hover:text-primary"
+                                  onClick={() => setSelectedRep(_id)}
+                                />
+                              </td>
+                            </tr>
+                          )
+                        )}
+                      </>
+                    )}
+                  </>
+                )}
               </tbody>
             </table>
-            {((loading)) &&
-              (<article className="flex items-center justify-center w-full h-[28rem]">
-                <ClipLoader
-                  color='#25a2b5'
-                  size={70}
-                />
-              </article>)}
+            {loading && (
+              <article className="flex items-center justify-center w-full h-[28rem]">
+                <ClipLoader color="#25a2b5" size={70} />
+              </article>
+            )}
           </div>
 
           <div className="flex justify-between items-center mt-4">
-            <div className="text-gray-500">Página {page}/{pages}</div>
-            {(pages > 1) && (
+            <div className="text-gray-500">
+              Página {page}/{pages}
+            </div>
+            {pages > 1 && (
               <div className="flex space-x-2">
-                <button className="p-2 bg-primary hover:bg-darkprim text-white rounded-md"
+                <button
+                  className="p-2 bg-primary hover:bg-darkprim text-white rounded-md"
                   onClick={loadLast}
                   disabled={page === 1}
                 >
-                  {'<'}
+                  {"<"}
                 </button>
-                <button className="p-2 bg-primary hover:bg-darkprim text-white rounded-md"
+                <button
+                  className="p-2 bg-primary hover:bg-darkprim text-white rounded-md"
                   onClick={loadNext}
                   disabled={page === pages}
                 >
-                  {'>'}
+                  {">"}
                 </button>
               </div>
             )}
--- elephantalk-moderation/src/components/interface/Navbar.jsx
+++ elephantalk-moderation/src/components/interface/Navbar.jsx
@@ -1,57 +1,65 @@
-'use client'
+"use client";
 
 import { useSession, signOut } from "next-auth/react";
-import React from "react"
+import React from "react";
 import { RiFileWarningFill } from "react-icons/ri";
 import { RiFileHistoryFill } from "react-icons/ri";
 import { BeatLoader } from "react-spinners";
 
-export const Navbar = React.memo(({
-    activePage,
-    setActivePage,
-}) => {
-    const session = useSession();
+export const Navbar = React.memo(({ activePage, setActivePage }) => {
+  const session = useSession();
 
-    // Default active page
+  // Default active page
 
+  return (
+    <header className="flex items-center justify-between bg-white p-4 shadow-md">
+      <div className="flex items-center">
+        <img
+          src="/assets/logo.webp"
+          alt="ElephanTalk Logo"
+          className="h-8 mr-2"
+        />
+      </div>
+      <nav className="flex space-x-4">
+        <button
+          className={`flex items-center p-2 text-gray-700 hover:text-teal-500 ${
+            activePage === "monitor" ? "border-b-2 border-teal-500" : ""
+          }`}
+          onClick={() => setActivePage("monitor")}
+        >
+          <RiFileWarningFill className="text-2xl mr-2" />{" "}
+          {/* Adjust the font size and margin */}
+          <span className="text-base">monitor</span>{" "}
+          {/* Adjust the text size if needed */}
+        </button>
+        <button
+          className={`flex items-center p-2 text-gray-700 hover:text-teal-500 ${
+            activePage === "history" ? "border-b-2 border-teal-500" : ""
+          }`}
+          onClick={() => setActivePage("history")}
+        >
+          <RiFileHistoryFill className="text-2xl mr-2" />{" "}
+          {/* Adjust the font size and margin */}
+          <span className="text-base">historial</span>{" "}
+          {/* Adjust the text size if needed */}
+        </button>
 
-    return (
-        <header className="flex items-center justify-between bg-white p-4 shadow-md" >
-            <div className="flex items-center">
-                <img src="/assets/logo.webp" alt="ElephanTalk Logo" className="h-8 mr-2" />
-            </div>
-            <nav className="flex space-x-4">
-                <button
-                    className={`flex items-center p-2 text-gray-700 hover:text-teal-500 ${activePage === 'monitor' ? 'border-b-2 border-teal-500' : ''}`}
-                    onClick={() => setActivePage('monitor')}
-                >
-                    <RiFileWarningFill className="text-2xl mr-2" /> {/* Adjust the font size and margin */}
-                    <span className="text-base">monitor</span> {/* Adjust the text size if needed */}
-                </button>
-                <button
-                    className={`flex items-center p-2 text-gray-700 hover:text-teal-500 ${activePage === 'history' ? 'border-b-2 border-teal-500' : ''}`}
-                    onClick={() => setActivePage('history')}
-                >
-                    <RiFileHistoryFill className="text-2xl mr-2" /> {/* Adjust the font size and margin */}
-                    <span className="text-base">historial</span> {/* Adjust the text size if needed */}
-                </button>
-                
-                <button className="flex items-center p-2 text-gray-700 hover:text-teal-500"
-                    onClick={() => signOut({ callbackUrl: "/login" })}
-                >
-                    <span className="mr-2">historial</span>
-                </button>
-            </nav>
-            <div className="text-gray-700">
-                {(session?.data?.user?.name === undefined) ?
-                (<BeatLoader 
-                    color='#25a2b5'
-                    size={10}
-                />) : (`${session?.data?.user?.name} ${session?.data?.user?.lastname}`)}
-            </div>
-        </header >
-    );
+        <button
+          className="flex items-center p-2 text-gray-700 hover:text-teal-500"
+          onClick={() => signOut({ callbackUrl: "/login" })}
+        >
+          <span className="mr-2">historial</span>
+        </button>
+      </nav>
+      <div className="text-gray-700">
+        {session?.data?.user?.name === undefined ? (
+          <BeatLoader color="#25a2b5" size={10} />
+        ) : (
+          `${session?.data?.user?.name} ${session?.data?.user?.lastname}`
+        )}
+      </div>
+    </header>
+  );
 });
-
 
 Navbar.displayName = "Navbar";
